7fa001e9fbbb18fdf3272e7f357e144bd9e47c9d
Ogu <august.gros@gmail.com>
2022-05-02 17:54:08 +0200

    modif background, menu, shadow et border radius

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundSize: {
+        'phone': '100% 25%',
+        'max': '100% 30%',
+      },
+      backgroundImage: {
+        'small-custom': "url('../assets/images/bg_image_small.jpg')",
+        'full-custom': "url('../assets/images/bg_image_full.jpg')",
+      },
+      backgroundColor: {
+        'main': "#FFFAFF",
+      }
+    },
   },
   plugins: [],
 }
